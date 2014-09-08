8fc6e65d2ef955de57fdcc9cae452957d9f5fd5c
AlfonsoMonroyIV <alfonso.monroy.ti@gmail.com>
2014-09-08 12:06:58 -0500

    Nuevos Tutoriales

--- laravel/app/views/home/portafolio.blade.php
+++ laravel/app/views/home/portafolio.blade.php
@@ -6,13 +6,14 @@
 			
 			<ul class="portfolio-nav clearfix">
 				<li class="segment-1 selected-1"><a href="#" data-value="all">Todo</a></li>
-				<li class="segment-0"><a href="#" data-value="hola_mundo"><span>01</span>Hola Mundo (CRUD - GIT)</a></li>
+				<li class="segment-2"><a href="#" data-value="desarrollos"><span>01</span>Desarrollos</a></li>
+				<li class="segment-0"><a href="#" data-value="tutoriales"><span>02</span>Tutoriales de Laravel</a></li>
 				<!--li class="segment-4 last"><a href="#" data-value="ejemplox"><span>02</span>Ejemplo</a></li-->
 			</ul>
 			
 			<ul id="list" class="portfolio clearfix">
 			
-				<li data-id="id-1" class="hola_mundo">
+				<li data-id="id-1" class="desarrollos">
 					<img title="" alt="" src="{{asset('assets/home/img/sample/crud-min.gif')}}">
 					<span class="portfolio-overlay"></span>
 					<div class="info">
@@ -22,7 +23,7 @@
 						<div class="visit"><a href="{{URL::to('crud')}}" title="Live Demo"></a></div>
 					</div>				
 				</li>
-				<li data-id="id-20" class="hola_mundo">					
+				<li data-id="id-3" class="tutoriales">					
 					<img title="" alt="" src="{{asset('assets/home/img/sample/laravel-welcome.png')}}">
 					<span class="portfolio-overlay"></span>		
 					<div class="info">
@@ -31,6 +32,24 @@
 						<div class="play"><a href="https://www.youtube.com/watch?v=qFIvuD0u454&feature=youtu.be" rel="prettyPhoto[mixed]"></a></div>
 					</div>	
 				</li>
+				<li data-id="id-4" class="tutoriales">					
+					<img title="" alt="" src="{{asset('assets/home/img/sample/laravel-db.png')}}">
+					<span class="portfolio-overlay"></span>		
+					<div class="info">
+						<p class="info-title">Bases de datos Laravel</p>
+						<div class="read-more"><a href="http://laravel.com/docs/database" title="Read more"></a></div>
+						<div class="play"><a href="https://www.youtube.com/watch?v=4X9S7JvBsd4" rel="prettyPhoto[mixed]"></a></div>
+					</div>	
+				</li>
+				<li data-id="id-5" class="tutoriales">					
+					<img title="" alt="" src="{{asset('assets/home/img/sample/laravel-seed.png')}}">
+					<span class="portfolio-overlay"></span>		
+					<div class="info">
+						<p class="info-title">Poblar Base de Datos</p>
+						<div class="read-more"><a href="http://laravel.com/docs/migrations" title="Read more"></a></div>
+						<div class="play"><a href="https://www.youtube.com/watch?v=li4St-KpZQ0" rel="prettyPhoto[mixed]"></a></div>
+					</div>	
+				</li>								
 						
 
 						
